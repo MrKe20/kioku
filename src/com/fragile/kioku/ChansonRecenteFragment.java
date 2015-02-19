c36174bb4f00d9f29200ba67f7ca616fe8f1e00f
MrKe20 <kevngo26@hotmail.com>
2015-02-19 16:52:27 +0100

    tsiory est kawaii

---
+++ src/com/fragile/kioku/ChansonRecenteFragment.java
@@ -0,0 +1,21 @@
+package com.fragile.kioku;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ChansonRecenteFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view = inflater.inflate(R.id.frag, container);
+		return view;
+	}
+
+	
+	
+}
