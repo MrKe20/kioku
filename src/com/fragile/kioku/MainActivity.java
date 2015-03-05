20638f6240374f28ece44aa5a2b7c78c5cbfc40e
MrKe20 <kevngo26@hotmail.com>
2015-03-05 15:44:44 +0100

    login

--- src/com/fragile/kioku/MainActivity.java
+++ src/com/fragile/kioku/MainActivity.java
@@ -1,63 +1,57 @@
 package com.fragile.kioku;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+   private EditText  username=null;
+   private EditText  password=null;
+   private TextView attempts;
+   private Button login;
+   int counter = 3;
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      username = (EditText)findViewById(R.id.editText1);
+      password = (EditText)findViewById(R.id.editText2);
+      attempts = (TextView)findViewById(R.id.textView5);
+      attempts.setText(Integer.toString(counter));
+      login = (Button)findViewById(R.id.button1);
+   }
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+   public void login(View view){
+      if(username.getText().toString().equals("admin") && 
+      password.getText().toString().equals("admin")){
+      Toast.makeText(getApplicationContext(), "Redirecting...", 
+      Toast.LENGTH_SHORT).show();
+   }	
+   else{
+      Toast.makeText(getApplicationContext(), "Wrong Credentials",
+      Toast.LENGTH_SHORT).show();
+      attempts.setBackgroundColor(Color.RED);	
+      counter--;
+      attempts.setText(Integer.toString(counter));
+      if(counter==0){
+         login.setEnabled(false);
+      }
 
+   }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+}
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+   }
 
 }
